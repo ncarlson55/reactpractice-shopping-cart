008534d7a54e85327b449c6cb5e36ad85418ffbe
Nico Carlson <nicocarlson285@gmail.com>
2021-06-08 01:31:57 -0500

    Use State Created to update Cart Items block

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Basket from './components/Basket';
 import data from './data';
+import { useState } from 'react';
 
 function App() {
   const { products } = data;
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAdd = (product) => {
+    const exist = cartItems.find((item) => item.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id ? { ...exist, qty: exist.qty + 1 } : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...products, qty: 1 }]);
+    }
+  };
 
   return (
     <div className="App">
       <Header></Header>
       <div className="row">
-        <Main products={products}></Main>
-        <Basket></Basket>
+        <Main onAdd={onAdd} products={products}></Main>
+        <Basket onAdd={onAdd} cartItems={cartItems}></Basket>
       </div>
     </div>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 export default function Product(props) {
-    const {product} = props;
+    const {product, onAdd } = props;
     return (
         <div>
-            <img className="small picSize" src={product.image} alt={product.name} />
+            <img className="small" src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <div>${product.price}</div>
+            <div>
+                <button onClick={onAdd}> Add to Cart </button>
+            </div>
         </div>
     )
 }
